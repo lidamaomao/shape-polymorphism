e8c306d21c03c51cc231cf67213b2321a44587a4
JayChenFE <chenjiehwy@163.com>
2019-06-30 20:39:41 +0800

    圆的面积是半径的平方不是直径 (#2)
    圆的面积是半径的平方不是直径 (#2)

--- src/main/java/com/github/hcsp/polymorphism/Rectangle.java
+++ src/main/java/com/github/hcsp/polymorphism/Rectangle.java
@@ -1,6 +1,6 @@
 package com.github.hcsp.polymorphism;
 
-public class Rectangle {
+public class Rectangle extends Shape {
     // 长方形的长
     private double a;
     // 长方形的宽
@@ -10,4 +10,11 @@ public class Rectangle {
         this.a = a;
         this.b = b;
     }
+
+    @Override
+    public double getArea() {
+
+        return a * b;
+    }
+
 }
