2b9196a9973b5cf2f8af9869c92eca0db13c6092
东方来客 <39690418+westee@users.noreply.github.com>
2019-07-05 21:01:35 +0800

    堕胎 (#10)
    堕胎 (#10)

--- src/main/java/com/github/hcsp/polymorphism/Rectangle.java
+++ src/main/java/com/github/hcsp/polymorphism/Rectangle.java
@@ -1,6 +1,6 @@
 package com.github.hcsp.polymorphism;
 
-public class Rectangle {
+public class Rectangle extends Shape {
     // 长方形的长
     private double a;
     // 长方形的宽
@@ -9,5 +9,10 @@ public class Rectangle {
     public Rectangle(double a, double b) {
         this.a = a;
         this.b = b;
+    }
+
+    @Override
+    public double getArea(){
+        return this.a * this.b;
     }
 }
