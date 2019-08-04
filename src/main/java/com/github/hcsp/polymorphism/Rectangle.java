3f95c65a9f9d19643da0ba27a108c1809690f50c
frank-0908 <52615871+frank-0908@users.noreply.github.com>
2019-08-04 22:16:49 +0800

    创建多态的父类和子类 (#28)
    创建多态的父类和子类 (#28)

--- src/main/java/com/github/hcsp/polymorphism/Rectangle.java
+++ src/main/java/com/github/hcsp/polymorphism/Rectangle.java
@@ -1,6 +1,6 @@
 package com.github.hcsp.polymorphism;
 
-public class Rectangle {
+public class Rectangle extends Shape {
     // 长方形的长
     private double a;
     // 长方形的宽
@@ -9,5 +9,9 @@ public class Rectangle {
     public Rectangle(double a, double b) {
         this.a = a;
         this.b = b;
+    }
+    @Override
+    public double getArea() {
+        return a*b;
     }
 }
