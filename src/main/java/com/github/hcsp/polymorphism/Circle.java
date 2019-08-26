c9373bbecc652b081153a8ff7e19a9e569a8c622
PingAn_NetBar <840495823@qq.com>
2019-08-27 02:13:48 +0800

    继承并利用多态输出面积 (#41)
    继承并利用多态输出面积 (#41)

--- src/main/java/com/github/hcsp/polymorphism/Circle.java
+++ src/main/java/com/github/hcsp/polymorphism/Circle.java
@@ -1,11 +1,19 @@
 package com.github.hcsp.polymorphism;
 
-public class Circle {
+import static java.lang.Math.*;
+
+public class Circle extends Shape{
     // 圆的直径
     // 提示，圆周率可以使用Math.PI
     private double diameter;
 
+
     public Circle(double diameter) {
         this.diameter = diameter;
+    }
+    @Override
+    public double getArea() {
+        double radius = diameter/2;
+        return  PI*(radius*radius);
     }
 }
