e3dca0a440b8b52dda0879b0195e610597b009ef
LU YI ZHE <42718757+sceraser@users.noreply.github.com>
2019-08-31 09:40:23 +0800

    计算面积 (#43)
    计算面积 (#43)

--- src/main/java/com/github/hcsp/polymorphism/Rectangle.java
+++ src/main/java/com/github/hcsp/polymorphism/Rectangle.java
@@ -1,13 +1,19 @@
 package com.github.hcsp.polymorphism;
 
-public class Rectangle {
+public class Rectangle extends Shape{
     // 长方形的长
     private double a;
     // 长方形的宽
     private double b;
 
-    public Rectangle(double a, double b) {
-        this.a = a;
-        this.b = b;
+    Rectangle(double a, double b){
+        this.a=a;
+        this.b=b;
     }
+
+    @Override
+    public double getArea() {
+        return a*b;
+    }
+
 }
