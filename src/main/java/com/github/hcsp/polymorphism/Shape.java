9365cf30eb615925bf2302a76a723e0e908addf9
zhy533726735220 <809775352@qq.com>
2019-12-05 09:43:51 +0800

    Java面向对象初步练习：创建多态的父类和子类 (#79)
    Java面向对象初步练习：创建多态的父类和子类 (#79)

--- src/main/java/com/github/hcsp/polymorphism/Shape.java
+++ src/main/java/com/github/hcsp/polymorphism/Shape.java
@@ -1,8 +1,9 @@
 package com.github.hcsp.polymorphism;
 
 public class Shape {
+
     // 返回当前"形状"(Shape)的面积
-    public double getArea() {
+    protected double getArea() {
         return 0d;
     }
 }
