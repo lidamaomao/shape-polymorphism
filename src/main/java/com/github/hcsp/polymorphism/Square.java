d1a03395b24f3a91e60e9ec1cbab2c7fee6389d6
Felix Mi <41504852+mm123mm123@users.noreply.github.com>
2020-01-11 21:02:38 +0800

    多态练习 (#105)
    多态练习 (#105)

--- src/main/java/com/github/hcsp/polymorphism/Square.java
+++ src/main/java/com/github/hcsp/polymorphism/Square.java
@@ -1,10 +1,16 @@
 package com.github.hcsp.polymorphism;
 
-public class Square {
+public class Square extends Shape{
     // 正方形的边长
     private double sideLength;
 
     public Square(double sideLength) {
+
         this.sideLength = sideLength;
     }
+    @Override
+    public double getArea(){
+        return(sideLength*sideLength);
+    }
+
 }
