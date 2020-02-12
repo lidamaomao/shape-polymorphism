1ef7b29a58eea36904cd8059e9db0568be0658ef
RunZe Zheng <58139571+zrzReyesTEI@users.noreply.github.com>
2020-02-12 22:34:27 +0800

    shape polymorphism (#116)
    shape polymorphism (#116)

--- src/main/java/com/github/hcsp/polymorphism/Square.java
+++ src/main/java/com/github/hcsp/polymorphism/Square.java
@@ -1,10 +1,14 @@
 package com.github.hcsp.polymorphism;
 
-public class Square {
+public class Square extends Shape {
     // 正方形的边长
     private double sideLength;
 
     public Square(double sideLength) {
         this.sideLength = sideLength;
+    }
+    @Override
+    public double getArea() {
+        return Math.pow(sideLength, 2);
     }
 }
