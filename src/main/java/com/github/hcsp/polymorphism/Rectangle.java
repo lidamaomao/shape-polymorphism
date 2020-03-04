22acedafc1a5b18efe5017336417b7bb3f20031d
Airtoit <51944383+Airtoit@users.noreply.github.com>
2020-03-04 18:25:37 +0800

    利用多态计算面积 (#128)
    利用多态计算面积 (#128)

--- src/main/java/com/github/hcsp/polymorphism/Rectangle.java
+++ src/main/java/com/github/hcsp/polymorphism/Rectangle.java
@@ -1,13 +1,18 @@
 package com.github.hcsp.polymorphism;
 
-public class Rectangle {
-    // 长方形的长
-    private double a;
-    // 长方形的宽
-    private double b;
+public class Rectangle extends Shape {
+	// 长方形的长
+	private double a;
+	// 长方形的宽
+	private double b;
 
-    public Rectangle(double a, double b) {
-        this.a = a;
-        this.b = b;
-    }
+	public Rectangle(double a, double b) {
+		this.a = a;
+		this.b = b;
+	}
+
+	@Override
+	public double getArea() {
+		return a * b;
+	}
 }
