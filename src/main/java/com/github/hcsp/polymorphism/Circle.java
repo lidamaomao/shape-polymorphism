54a2fa8637bf2544c7562c95d6a51005df50f71b
何佳棋 <1149752574@qq.com>
2020-03-30 14:15:49 +0800

    创建多态的父类和子类 (#145)
    创建多态的父类和子类 (#145)

--- src/main/java/com/github/hcsp/polymorphism/Circle.java
+++ src/main/java/com/github/hcsp/polymorphism/Circle.java
@@ -1,11 +1,22 @@
 package com.github.hcsp.polymorphism;
 
-public class Circle {
+import static java.lang.Math.PI;
+
+public class Circle extends Shape{
+    public double getDiameter() {
+        return diameter;
+    }
+
     // 圆的直径
     // 提示，圆周率可以使用Math.PI
     private double diameter;
 
     public Circle(double diameter) {
         this.diameter = diameter;
+    }
+
+    @Override
+    public double getArea() {
+        return  Math.PI*getDiameter()*getDiameter()/4;
     }
 }
