5b0d6d74b695732822fef3354d17b0796dea771b
weiranyi <67884344+weiranyi@users.noreply.github.com>
2020-12-05 22:46:18 +0800

    纠正了复制圆类的公式后直接粘贴到正方形和长方形没有变更的情况 (#197)
    纠正了复制圆类的公式后直接粘贴到正方形和长方形没有变更的情况 (#197)

--- src/main/java/com/github/hcsp/polymorphism/Rectangle.java
+++ src/main/java/com/github/hcsp/polymorphism/Rectangle.java
@@ -1,13 +1,18 @@
 package com.github.hcsp.polymorphism;
 
-public class Rectangle {
+public class Rectangle extends Shape {
     // 长方形的长
     private double a;
     // 长方形的宽
     private double b;
 
+    public double getArea() {
+        return a * b;
+    }
+
     public Rectangle(double a, double b) {
         this.a = a;
         this.b = b;
     }
+
 }
--- src/main/java/com/github/hcsp/polymorphism/Square.java
+++ src/main/java/com/github/hcsp/polymorphism/Square.java
@@ -1,10 +1,15 @@
 package com.github.hcsp.polymorphism;
 
-public class Square {
+public class Square extends Shape {
     // 正方形的边长
     private double sideLength;
+
+    public double getArea() {
+        return sideLength * sideLength;
+    }
 
     public Square(double sideLength) {
         this.sideLength = sideLength;
     }
+
 }
