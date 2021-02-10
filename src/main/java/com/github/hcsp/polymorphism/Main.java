3700c9105df59a7295002693b57977a4eb940285
Apolo Du <dudueasy@126.com>
2021-02-10 16:50:59 +0800

    update (#205)
    update (#205)

--- src/main/java/com/github/hcsp/polymorphism/Main.java
+++ src/main/java/com/github/hcsp/polymorphism/Main.java
@@ -6,7 +6,9 @@ public class Main {
     // 正方形Square、长方形Rectangle、圆Circle为子类
     // 子类覆盖父类求面积的多态方法getArea()
     public static void main(String[] args) {
-        Shape[] shapes = new Shape[] {new Square(1.0), new Rectangle(1.0, 2.0), new Circle(1.0)};
+        Shape[] shapes = new Shape[]{
+                new Square(1.0), new Rectangle(1.0, 2.0), new Circle(1.0)
+        };
         for (Shape shape : shapes) {
             System.out.println("" + shape.getClass().getSimpleName() + "的面积是：" + shape.getArea());
         }
