55db9f3fb286ea34e424b5ff98660b0815a32eeb
MongieLee <le30n@outlook.com>
2021-10-26 16:21:51 +0800

    创建多态的父类和子类 (#228)
    创建多态的父类和子类 (#228)

--- src/main/java/com/github/hcsp/polymorphism/Shape.java
+++ src/main/java/com/github/hcsp/polymorphism/Shape.java
@@ -1,8 +1,6 @@
 package com.github.hcsp.polymorphism;
 
-public class Shape {
+public interface Shape {
     // 返回当前"形状"(Shape)的面积
-    public double getArea() {
-        return 0d;
-    }
+    double getArea();
 }
