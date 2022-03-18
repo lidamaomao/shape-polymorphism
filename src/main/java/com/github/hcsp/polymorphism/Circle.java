155203b6cb63d9f689d7a3b836bf2741374f0b29
yangbisheng <2606424906@qq.com>
2022-03-18 22:19:13 +0800

    创建多态的父类和子类 (#240)
    创建多态的父类和子类 (#240)

--- src/main/java/com/github/hcsp/polymorphism/Circle.java
+++ src/main/java/com/github/hcsp/polymorphism/Circle.java
@@ -1,11 +1,25 @@
 package com.github.hcsp.polymorphism;
 
-public class Circle {
+public class Circle extends Shape {
     // 圆的直径
     // 提示，圆周率可以使用Math.PI
     private double diameter;
 
-    public Circle(double diameter) {
+    public double getDiameter() {
+        return diameter;
+    }
+
+    public void setDiameter(double diameter) {
         this.diameter = diameter;
+    }
+
+    public Circle(double diameter) {
+
+        this.setDiameter(diameter);
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * (getDiameter() / 2) * (getDiameter() / 2);
     }
 }
--- src/main/java/com/github/hcsp/polymorphism/Rectangle.java
+++ src/main/java/com/github/hcsp/polymorphism/Rectangle.java
@@ -1,13 +1,34 @@
 package com.github.hcsp.polymorphism;
 
-public class Rectangle {
+public class Rectangle extends Shape {
     // 长方形的长
     private double a;
     // 长方形的宽
     private double b;
 
-    public Rectangle(double a, double b) {
+    public double getA() {
+        return a;
+    }
+
+    public void setA(double a) {
         this.a = a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public void setB(double b) {
         this.b = b;
+    }
+
+    public Rectangle(double a, double b) {
+        this.setA(a);
+        this.setB(b);
+    }
+
+    @Override
+    public double getArea() {
+        return a * b;
     }
 }
