819d842a4332f4fe96ea1030d170a79ff2a57489
Y.L <100324074+xiangqiangkan@users.noreply.github.com>
2022-03-28 15:42:22 +0800

    多态 (#243)
    多态 (#243)

--- src/main/java/com/github/hcsp/polymorphism/Circle.java
+++ src/main/java/com/github/hcsp/polymorphism/Circle.java
@@ -1,6 +1,7 @@
 package com.github.hcsp.polymorphism;
 
-public class Circle {
+public class Circle extends Shape
+{
     // 圆的直径
     // 提示，圆周率可以使用Math.PI
     private double diameter;
@@ -8,4 +9,7 @@ public class Circle {
     public Circle(double diameter) {
         this.diameter = diameter;
     }
+    @Override
+    public double getArea() {
+        return (diameter/2)*(diameter/2)*Math.PI;}
 }
