594fafa871543b3f59c28610a6262077dbac7499
Xing Liu <94578246+iamxliu@users.noreply.github.com>
2022-04-06 14:23:52 +0800

    2 (#245)
    2 (#245)

--- src/main/java/com/github/hcsp/polymorphism/Circle.java
+++ src/main/java/com/github/hcsp/polymorphism/Circle.java
@@ -1,9 +1,16 @@
 package com.github.hcsp.polymorphism;
 
-public class Circle {
+public class Circle extends Shape {
     // 圆的直径
     // 提示，圆周率可以使用Math.PI
     private double diameter;
+
+    @Override
+    public double getArea() {
+        double d = diameter/2;
+        return Math.PI * d * d;
+    }
+
 
     public Circle(double diameter) {
         this.diameter = diameter;
