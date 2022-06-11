c7c83f9abfcebd48222fcac89bcc5e3290affdfd
Ye Shiqing <908202572@qq.com>
2022-06-11 17:44:01 +0800

    创建多态的父类和子类 (#252)
    创建多态的父类和子类 (#252)

--- src/main/java/com/github/hcsp/polymorphism/Circle.java
+++ src/main/java/com/github/hcsp/polymorphism/Circle.java
@@ -1,11 +1,18 @@
 package com.github.hcsp.polymorphism;
 
-public class Circle {
+public class Circle extends Shape {
     // 圆的直径
     // 提示，圆周率可以使用Math.PI
     private double diameter;
+    private double radius;
 
     public Circle(double diameter) {
         this.diameter = diameter;
+        this.radius = diameter / 2;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
     }
 }
